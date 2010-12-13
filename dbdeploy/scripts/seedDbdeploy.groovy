1462d40d2b0d82fcc6fe7fe82a7b0ae1e806c8a9
rajiv.narula@gmail.com <rajiv.narula@gmail.com@fad8b2dc-92d9-a717-7a00-eaca39fd1c39>
2010-12-13 00:26:16 +0000

    Changing names

--- dbdeploy/scripts/seedDbdeploy.groovy
+++
@@ -1,5 +0,0 @@
-def ant = new AntBuilder()
-ant.sequential {
-	sql(driver:'oracle.jdbc.driver.OracleDriver', url:'jdbc:oracle:thin:@localhost:1521:XE',userid:'trackingdbd',password:'l0bst3r'){
-	fileset(file:'createSchemaVersionTable.ora.sql')
-}
