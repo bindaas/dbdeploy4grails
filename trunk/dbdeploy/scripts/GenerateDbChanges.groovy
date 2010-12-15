45b749cf6b595a33a3f096a672f9aaabcdf051a3
rajiv.narula@gmail.com <rajiv.narula@gmail.com@fad8b2dc-92d9-a717-7a00-eaca39fd1c39>
2010-12-15 16:25:19 +0000

    uses the config file now

--- trunk/dbdeploy/scripts/GenerateDbChanges.groovy
+++ trunk/dbdeploy/scripts/GenerateDbChanges.groovy
@@ -2,22 +2,54 @@ import com.dbdeploy.exceptions.UsageException
 import com.dbdeploy.DbDeploy
 import com.dbdeploy.DbDeployCommandLineParser
 
-def dbUser = 'TRACKINGDBD'
-def dbPassword = 'l0bst3r'
-def dbURL = 'jdbc:oracle:thin:@localhost:1521:XE'
-def dbSID ='XE'
-def outputFle = 'consolidated_script.sql'
-def applyChanges = 'true'
-def dbDriver = 'oracle.jdbc.driver.OracleDriver'
-def dbType = 'ora'
-def scripts = '/my/SourceCode/tracking-platform/scripts/sql'
+grailsHome = Ant.project.properties."environment.GRAILS_HOME"    
+includeTargets << new File ( "${grailsHome}/scripts/Bootstrap.groovy" )
 
-def params = ['-U' ,dbUser,'-P', dbPassword,'-D' ,dbDriver,'-u' ,dbURL,'-d', dbType ,'-o' ,outputFle,'-s' ,scripts ] as String[]
+Properties dsProps = new Properties()
+Properties dbdProps = new Properties()
 
-DbDeployCommandLineParser commandLineParser = new DbDeployCommandLineParser();
-DbDeploy dbDeploy = new DbDeploy();
-commandLineParser.parse(params, dbDeploy);
-dbDeploy.go();
+def populateDSProperties = {
 
-target(default:"Do nothing target") {
+  File dsFile = new File("${basedir}/grails-app/conf/DataSource.groovy")
+  def dsConfig = null
+  if (dsFile.exists()) {
+    dsConfig = new ConfigSlurper(grailsEnv).parse(dsFile.text)
+  }
+
+  dsProps.'db.username' = dsConfig?.dataSource?.username 
+  dsProps.'db.password' = dsConfig?.dataSource?.password 
+  dsProps.'db.driver' = dsConfig?.dataSource?.driverClassName 
+  dsProps.'db.url' = dsConfig?.dataSource?.url 
+
 }
+
+def populateDBDProperties = {
+
+  File dbdFile = new File("${basedir}/grails-app/conf/DbdeployConfig.groovy")
+  def dbdConfig = null
+  if (dbdFile.exists()) {
+    dbdConfig = new ConfigSlurper(grailsEnv).parse(dbdFile.text)
+  }
+
+  dbdProps.'dbd.outputFle' = dbdConfig?.output?.consolidatedScriptFle 
+  dbdProps.'dbd.undoOutputFle' = dbdConfig?.output?.consolidatedUndoScriptFle 
+  dbdProps.'dbd.scripts' = dbdConfig?.dbdeploy?.sqlFolder 
+  dbdProps.'dbd.dbType' = dbdConfig?.dbdeploy?.dbType 
+
+}
+
+target('default':"Slurp properties") {
+	populateDSProperties ()
+	populateDBDProperties ()
+
+def params = ['-U' ,dsProps.'db.username','-P', dsProps.'db.password','-D' ,dsProps.'db.driver','-u' ,dsProps.'db.url','-d', dbdProps.'dbd.dbType' ,'-o' ,dbdProps.'dbd.outputFle','-s' ,dbdProps.'dbd.scripts' ] as String[]
+
+	DbDeployCommandLineParser commandLineParser = new DbDeployCommandLineParser();
+	DbDeploy dbDeploy = new DbDeploy();
+	commandLineParser.parse(params, dbDeploy);
+	//dbDeploy.setUndoOutputfile(new java.io.File(undoOutputFle))
+	dbDeploy.go();
+
+}
+
+setDefaultTarget('default')
