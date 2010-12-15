4c4e191259bf17b82e5b503007791b7d8e8a57d5
rajiv.narula@gmail.com <rajiv.narula@gmail.com@fad8b2dc-92d9-a717-7a00-eaca39fd1c39>
2010-12-15 16:53:58 +0000

    Segregating files and folders

--- dbdeploy/scripts/InitializeDbdeploy.groovy
+++ dbdeploy/scripts/InitializeDbdeploy.groovy
@@ -4,6 +4,8 @@ grailsHome = Ant.project.properties."environment.GRAILS_HOME"
 includeTargets << new File ( "${grailsHome}/scripts/Bootstrap.groovy" )
 
 Properties props = new Properties()
+Properties dbdProps = new Properties()
+
 
 def populateProperties = {
 
@@ -19,13 +21,26 @@ def populateProperties = {
   props.'db.url' = dsConfig?.dataSource?.url 
 
 }
+
+def populateDBDProperties = {
+
+  File dbdFile = new File("${basedir}/grails-app/conf/DbdeployConfig.groovy")
+  def dbdConfig = null
+  if (dbdFile.exists()) {
+    dbdConfig = new ConfigSlurper(grailsEnv).parse(dbdFile.text)
+  }
+
+  dbdProps.'dbd.dbType' = dbdConfig?.dbdeploy?.dbType 
+}
+
   
 target('default':"Initialize DBDeploy") {
 	populateProperties ()
+	populateDBDProperties ()
 	
 	ant.sequential {
 		sql(driver:props.'db.driver', url:props.'db.url', userid:props.'db.username', password:props.'db.password'){
-			fileset(file:'${basedir}/grails-app/dbdeploy/createSchemaVersionTable.ora.sql')
+			fileset(file:'${basedir}/grails-app/dbdeploy/initialize/createSchemaVersionTable.'+dbdProps.'dbd.dbType'+'.sql')
 		}
 	}	
 
