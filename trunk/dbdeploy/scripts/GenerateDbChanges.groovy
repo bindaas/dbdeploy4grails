b63ce409ddc2017e73d0aa462de9e4a63351d3a2
rajiv.narula@gmail.com <rajiv.narula@gmail.com@fad8b2dc-92d9-a717-7a00-eaca39fd1c39>
2010-12-15 14:11:03 +0000

    Generates DB changes

--- trunk/dbdeploy/scripts/GenerateDbChanges.groovy
+++ trunk/dbdeploy/scripts/GenerateDbChanges.groovy
@@ -2,17 +2,22 @@ import com.dbdeploy.exceptions.UsageException
 import com.dbdeploy.DbDeploy
 import com.dbdeploy.DbDeployCommandLineParser
 
-def dbUser = '?'
-def dbPassword = '?'
+def dbUser = 'TRACKINGDBD'
+def dbPassword = 'l0bst3r'
 def dbURL = 'jdbc:oracle:thin:@localhost:1521:XE'
 def dbSID ='XE'
 def outputFle = 'consolidated_script.sql'
 def applyChanges = 'true'
 def dbDriver = 'oracle.jdbc.driver.OracleDriver'
+def dbType = 'ora'
+def scripts = '/my/SourceCode/tracking-platform/scripts/sql'
 
-def params = ['-U' ,dbUser,'-P', dbPassword,'-D' ,dbDriver,'-u' ,dbURL,'-d', 'ora' ,'-o' ,outputFle,'-s' ,'/my/SourceCode/tracking-platform/scripts/sql'] as String[]
+def params = ['-U' ,dbUser,'-P', dbPassword,'-D' ,dbDriver,'-u' ,dbURL,'-d', dbType ,'-o' ,outputFle,'-s' ,scripts ] as String[]
 
 DbDeployCommandLineParser commandLineParser = new DbDeployCommandLineParser();
 DbDeploy dbDeploy = new DbDeploy();
 commandLineParser.parse(params, dbDeploy);
 dbDeploy.go();
+
+target(default:"Do nothing target") {
+}
